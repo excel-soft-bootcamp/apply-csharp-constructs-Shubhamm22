8fe15248db8bc587ba3c600b651ccbef07a175e1
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-06-24 17:11:55 +0530

    Add files via upload

---
+++ BMIcalculator/bmi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMIcalculator
+{
+    class bmi
+    #region
+    {
+        private float kg;
+       private float m;
+       
+        public float Getkg()
+        { return this.kg; }
+        public float Getm()
+        { return this.m; }
+
+        #endregion
+
+
+
+
+    }
+
+}

7faca6eccf7312d88201ccb0e0e633195eb37a8b
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-06-24 20:25:26 +0530

    Add files via upload

---
+++ BMIcalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BMIcalculator
+{ 
+    //user defined datatype
+    class Program
+    {
+        #region
+        static void Main(string[] args)
+        {
+            //Instantiation
+            Bodyinformation inputdata = new Bodyinformation();
+            inputdata.Readingbodydetails();
+            bmicalculation  result = new bmicalculation();
+            result.Displaybmiresult();
+            
+
+
+
+           
+        }
+    }
+    #endregion
+}
---
+++ BMIcalculator/bmicalculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMIcalculator
+{
+    //user defined datatype
+    class bmicalculation
+    {
+        #region
+        //Fields
+        float bmiCalculation;
+
+        //Function
+        public void Displaybmiresult()
+        {
+            //instantiation
+            Bodyinformation bodydata = new Bodyinformation();
+            bmiCalculation = bodydata.getkilogramvalue / bodydata.getmetervalue;
+            if (bmiCalculation < 18.5)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else if (bmiCalculation >= 25)
+            {
+                Console.WriteLine("Healthy");
+            }
+        }
+    }
+    #endregion
+}

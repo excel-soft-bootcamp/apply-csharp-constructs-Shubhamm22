8dc0acbc23f9cddace75c045a8f2998a10851a02
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-06-26 12:37:40 +0530

    Add files via upload

---
+++ BmiCalculationProgram1/BmiCalculation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BmiCalculationProgram1
+{
+    //user defined datatype
+    class BmiCalculation
+    {
+        #region
+       
+        //Function
+        public  void  Calculate(float bmiCalculate)
+            {
+                //instantiation
+                BodyInformation bodydata = new BodyInformation();
+                bmiCalculate = bodydata.KilogramValue / bodydata.MeterValue;
+
+            }
+        
+        
+        #endregion
+    }
+}
+
---
+++ BmiCalculationProgram1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BmiCalculationProgram1
+{
+    class Program
+    {
+        #region
+        static void Main(string[] args)
+        {
+           
+                BmiDisplay _instance = new BmiDisplay();
+                _instance.ConsoleDisplay();
+        }
+        #endregion
+    }
+}
+    
+

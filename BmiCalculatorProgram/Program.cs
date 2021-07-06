9c720c6feedbd9b664f680b162e174dcdb5503a9
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 15:21:56 +0530

    Add files via upload

---
+++ BmiCalculatorProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BmiCalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+
+
+            BodyInformation _bodyinformation = new BodyInformation();
+            float heightValue = _bodyinformation.MeterValue;
+            float weightValue = _bodyinformation.KilogramValue;
+
+            BmiCalculateLib.CalculateType.Calculate(heightValue, weightValue);
+           BmiValidateLib.ValidateType.Validate()
+                 
+           
+
+        }
+    }
+}

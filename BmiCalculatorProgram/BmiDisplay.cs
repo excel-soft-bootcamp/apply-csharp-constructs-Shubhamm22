018dbe86eb3d0580203c9bab2836f20916135f01
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 15:48:08 +0530

    Add files via upload

---
+++ BmiCalculatorProgram/BmiDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BmiCalculatorProgram
+{
+    class BmiDisplay
+    {
+        public void ConsoleDisplay(string result)
+        {
+            Console.WriteLine(result);
+
+        }
+    }
+}

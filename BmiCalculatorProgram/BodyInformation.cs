f1806786a1c01f32029e722bb47ee2b6f96b9ab8
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 15:46:25 +0530

    Add files via upload

---
+++ BmiCalculatorProgram/BodyInformation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BmiCalculatorProgram
+{
+    class BodyInformation
+    {
+        
+            
+            private float kilogramValue;
+            private float meterValue;
+
+            public float KilogramValue
+            {
+                get { return this.kilogramValue; }
+                set
+                {
+                    Console.WriteLine("enter the weight in kilograms");
+                    this.kilogramValue = float.Parse(Console.ReadLine());
+
+                }
+            }
+            public float MeterValue
+            {
+                get { return this.meterValue; }
+                set
+                {
+                    Console.WriteLine("enter the height in meters");
+                    this.meterValue = float.Parse(Console.ReadLine());
+                }
+            }
+
+
+        }
+
+    }
+

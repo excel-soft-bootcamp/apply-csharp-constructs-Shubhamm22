516c4b703af971befca9ed048d307fb9bf358433
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-06 15:54:46 +0530

    Add files via upload

---
+++ BmiCalculatorProgram/BmiCalculateLib/CalculateType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BmiCalculateLib
+{
+    public  class CalculateType
+    {
+        
+        public  float Calculate(float height, float weight)
+        {
+
+             
+                 return (weight / height);
+            
+
+        }
+       
+    }
+}

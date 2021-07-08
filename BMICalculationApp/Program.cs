4a6b03aefdeaae7efe2f9f2912049ef85a5d4fbb
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-08 16:26:02 +0530

    Add files via upload

---
+++ BMICalculationApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BMICalculationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value = default;
+            do
+            {
+
+
+                Console.WriteLine("Select Method to Display BMI");
+                Console.WriteLine("1.Console Input");
+                Console.WriteLine("2.File Input");
+                try
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                    if (value < 1 || value > 2)
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+
+
+                    else { break; }
+                }
+                catch (FormatException ex) { Console.WriteLine("Input only in number"); }
+            } while (true);
+            switch (value)
+            {
+                case 1:
+
+                    ConsoleInput _consoleInput = new ConsoleInput();
+                    _consoleInput.Display();
+                    break;
+                    case 2:
+
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ BMICalculationApp/ReadingCsv.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BMICalculationApp
+{
+    class ReadingCsv
+    {
+        public string name { get; set; }
+        public double height { get; set; }
+        public double weight { get; set; }
+
+        static void readFile()
+        {
+            var lines = File.ReadAllLines(@"C:\Users\shubham.m\Downloads\download\PatientData.csv");
+
+        }
+
+
+    }
+}
+

46a386d419c022dd4685db96e6e5c17ac9b12359
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-08 16:25:07 +0530

    Add files via upload

---
+++ BMICalculationApp/ConsoleInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMICalculationApp
+{
+    class ConsoleInput
+    {
+        public void Display()
+        {
+            BMIBodyInformationLib.BodyInformationType _bodyinformation = new BMIBodyInformationLib.BodyInformationType();
+            double _heightValue = _bodyinformation.ReadBodyInformation("enter your height");
+            double _weightValue = _bodyinformation.ReadBodyInformation("enter your weight");
+
+
+            BMICalculatorLib.BMICalculatorType _bmicalculate = new BMICalculatorLib.BMICalculatorType();
+            double _bmivalue = _bmicalculate.CalculateBMIResult(_heightValue, _weightValue);
+
+            BMIValidationLib.BMIValidatorType _bmivalidate = new BMIValidationLib.BMIValidatorType();
+            string _result = _bmivalidate.BMIValidate(_bmivalue);
+
+            BMIConsoleDisplayLib.DisplayType _display = new BMIConsoleDisplayLib.DisplayType();
+            _display.BMIDisplay(_result);
+        }
+
+    }
+}

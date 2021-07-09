87947ae7b4bb51bb1c1a8ab5bc0116a0f848bbb4
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-09 18:49:04 +0530

    Add files via upload

---
+++ DoorSecuritySystem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSecuritySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           SecuritySystem _observer = new SecuritySystem();
+            Statushandler _notify = new Statushandler(_observer.Sms);
+
+            DoorSensor _doorsensor = new DoorSensor();
+            _doorsensor.Statuschanged += _notify;
+           _doorsensor.Open();
+        }
+
+    }
+}
---
+++ DoorSecuritySystem/SecuritySystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSecuritySystem
+{
+    class SecuritySystem
+    {
+        public void Sms(string status)
+        {
+            Console.WriteLine(status);
+            
+        }
+    }
+}

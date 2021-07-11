50e3b9217a547381a639dfc0d978111ef91fdb8e
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-11 09:39:03 +0530

    Add files via upload

---
+++ DoorSecuritySystem/DoorSensor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSecuritySystem
+{
+    public delegate void Statushandler(string status);
+    class DoorSensor
+    {
+        string status = "idle";
+        public event Statushandler Statuschanged;
+        public void Open()
+        {
+            status= "open";
+            //Statuschanged.Invoke(this.status);
+            this.Notify();
+        }
+        public void Close() 
+        {
+            status = "close";
+            //Statuschanged.Invoke(this.status);
+            this.Notify();
+        }
+        void Notify()
+        {
+            string message = $"{this.status} and Time : {System.DateTime.Now.ToString()}";
+            Statuschanged.Invoke(message);
+        }
+    }
+}
